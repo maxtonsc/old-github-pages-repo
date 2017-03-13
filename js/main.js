973eb4c64972c421590f8b8fe2c8b8f1e1a8c0ef
maxtonsc <maxtonsc@gmail.com>
2017-03-13 18:22:29 +1300

    Fixed js and blog html for new blogpost

--- js/main.js
+++ js/main.js
@@ -12,12 +12,18 @@ $('.portfolio-filter').on( 'click', 'a', function(e) {
 
 window.onload = function(){
 	$container.isotope({filter:'*'});
+
+
 	$("#March12Button").click(function(){
 		blogpost("#March12Post");
 	});
 
 	$("#March3Button").click(function(){
 		blogpost("#March3Post");
+	});
+
+	$("#March13Button").click(function(){
+		blogpost("#March13Post");
 	});
 
 	$('.hover').mouseover(function() {
@@ -28,6 +34,7 @@ window.onload = function(){
 
 
 function blogpost($contentName){
+	$("#March13Post").css("display","none");
 	$("#March3Post").css("display","none");
 	$("#March12Post").css("display","none");
 	$($contentName).css("display","block");
